7542919014d70d7acbb5813289a21b910cc5af94
Wes McElroy <mcelroywes@gmail.com>
2019-07-11 17:46:49 -0700

    Preparing for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://wes-mcelroy@localhost/its-a-date',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://wes-mcelroy@localhost/its-a-date',
     JWT_SECRET: process.env.JWT_SECRET || 'this-jwt-secret'
 };
